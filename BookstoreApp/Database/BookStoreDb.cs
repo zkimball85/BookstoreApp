8dc99963f18e604d0e3dfc0d7650ef945adbbee5
Zac Kimball <z.kimball85@gmail.com>
2026-07-21 21:22:14 -0700

    Add BookStoreDb DbContext and update project file
    Added BookStoreDb class in BookstoreApp.Database, inheriting from DbContext and configured for SQL Server. Removed ItemGroup referencing the Database folder from BookstoreApp.csproj.

---
+++ BookstoreApp/Database/BookStoreDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookstoreApp.Database;
+
+public class BookStoreDb : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb\mssqllocaldb);Database=BookstoreDb; Trusted_Connection=True; TrustServerCertificate=True; ");
+    }
+}
+
