6e3104eb5baf5f0c4f75153692eb7cd0077b8e74
Zac Kimball <z.kimball85@gmail.com>
2026-07-28 13:38:05 -0700

    Add Genre model with validation and documentation
    Added a new Genre class to BookstoreApp.Models with GenreId and Name properties. Applied data annotations for validation and included XML documentation comments for clarity.

---
+++ BookstoreApp/Models/Genre.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookstoreApp.Models;
+
+/// <summary>
+/// Represents a genre of books in the bookstore.
+/// </summary>
+public class Genre
+{
+    /// <summary>
+    /// The primary key for the genre.
+    /// </summary>
+    [Key]
+    public int GenreId { get; set; }
+
+    /// <summary>
+    /// The name of the genre.
+    /// </summary>
+    [Required]
+    public required string Name { get; set; }
+}
+
