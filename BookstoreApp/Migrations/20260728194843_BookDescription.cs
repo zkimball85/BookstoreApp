badf673b505179954187dcb48f09dd75d18329d9
Zac Kimball <z.kimball85@gmail.com>
2026-07-28 12:52:49 -0700

    Add ISBN and description columns to Books table
    Added two EF Core migrations: AddISBN (non-nullable ISBN column) and BookDescription (nullable description column) to the Books table. Updated BookStoreDbModelSnapshot to reflect these schema changes.

---
+++ BookstoreApp/Migrations/20260728194843_BookDescription.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookstoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "Books");
+        }
+    }
+}
